15bf0a226ed13dafda78614711b7b923f2a06a40
Roberto B monteiro <jordeam@gmail.com>
2025-08-25 17:58:29 -0300

    Inserting existent component from BOM by partnumber

--- public/scripts/bom.js
+++ public/scripts/bom.js
@@ -40,21 +40,25 @@ function insertWithPartnumber(index) {
     body: new URLSearchParams({
       qty: form.elements.qty.value,
       comp_id: form.elements.comp_id.value,
+      sc_id: form.elements.sc_id.value,
       pn: form.elements.pn.value,
       ordercode: form.elements.ordercode.value,
+      rounding: form.elements.rounding.value,
       labels: form.elements.labels.value,
       supplier_id: form.elements.supplier_id.value,
       rounding: form.elements.rounding.value,
+      manufact_id: form.elements.manufact_id.value,
+      checked: form.elements.active.value,
     }).toString(),
   };
 
   const request = new Request('/bom/'+loc_id+'/insert', requestOptions);
 
   fetch(request)
-    .then(response => response.html())
+    .then(res => res.text())
     .then(data => {
       const divbom = document.getElementById('bom'+index);
-      divbom.innerHTML = '<p> Componente inserido</p>\n<hr>';
+      divbom.innerHTML = data;
     })
     .catch(error => console.error(error));
 }
